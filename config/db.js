3032153d21cb9cd0540d3e01c24e432dab5e7ab7
gautam-jasmeet <jasmeet.gautam.solar@gmail.com>
2024-10-17 14:54:50 +0530

    testing

--- config/db.js
+++ config/db.js
@@ -1,25 +1,3 @@
-// import mysql from "mysql2";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to MySQL:", err.message);
-//   } else {
-//     console.log("Connected to MySQL database".bgMagenta.white);
-//   }
-// });
-
-// export default db;
-
 import mysql from "mysql2/promise"; // Use promise-based API
 import dotenv from "dotenv";
 
